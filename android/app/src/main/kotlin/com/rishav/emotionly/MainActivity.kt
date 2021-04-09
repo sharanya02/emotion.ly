8be1e3402b437f93d512b5dd61af44a31a987b34
Rishav Naskar <rishavnaskar.r@gmail.com>
2021-04-10 01:20:39 +0530

    feat: initial commit

---
+++ android/app/src/main/kotlin/com/rishav/emotionly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rishav.emotionly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+
+Future main() async {
+  await DotEnv.load(fileName: ".env");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.white),
+          tooltipTheme:
+              TooltipThemeData(decoration: BoxDecoration(color: Colors.white))),
+      home: Container(),
+    );
+  }
+}
